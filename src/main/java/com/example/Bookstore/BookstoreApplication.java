1717849f838601b5b69f7a776b2609f90ab13034
AlishStrong <34649384+AlishStrong@users.noreply.github.com>
2018-02-19 21:24:38 +0200

    task 1, plus some changes in code

--- src/main/java/com/example/Bookstore/BookstoreApplication.java
+++ src/main/java/com/example/Bookstore/BookstoreApplication.java
@@ -1,12 +1,38 @@
 package com.example.Bookstore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.Bookstore.domain.BookRepository;
+import com.example.Bookstore.domain.Book;
+
 
 @SpringBootApplication
 public class BookstoreApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
+	
+	@Bean
+	public CommandLineRunner demo(BookRepository repository) {
+		return (args) -> {
+			log.info("Save a couple of books");
+			repository.save(new Book("War and Peace", "Leo Tolstoy", 1867, "9781566190274", 8.48));
+			repository.save(new Book("The last victim of war", "Utkur Hoshimov", 1967, "-", 9.98));
+			
+			log.info("fetch all books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
+		};
+	}
+	
 }
--- src/main/java/com/example/Bookstore/domain/Book.java
+++ src/main/java/com/example/Bookstore/domain/Book.java
@@ -1,59 +1,77 @@
 package com.example.Bookstore.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
-private String title;
-private String author;
-private int year;
-private int isbn;
-private boolean price;
 
-public Book(String title, String author, int year, int isbn, boolean price) {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String title;
+	private String author;
+	private int year;
+	private String isbn;
+	private double price;
+	
+	public Book() {}
 
-	this.title = title;
-	this.author = author;
-	this.year = year;
-	this.isbn = isbn;
-	this.price = price;
-}
+	public Book(String title, String author, int year, String isbn, double price) {
 
-public String getTitle() {
-	return title;
-}
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.isbn = isbn;
+		this.price = price;
+	}
 
-public void setTitle(String title) {
-	this.title = title;
-}
+	public String getTitle() {
+		return title;
+	}
 
-public String getAuthor() {
-	return author;
-}
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-public void setAuthor(String author) {
-	this.author = author;
-}
+	public String getAuthor() {
+		return author;
+	}
 
-public int getYear() {
-	return year;
-}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
 
-public void setYear(int year) {
-	this.year = year;
-}
+	public int getYear() {
+		return year;
+	}
 
-public int getIsbn() {
-	return isbn;
-}
+	public void setYear(int year) {
+		this.year = year;
+	}
 
-public void setIsbn(int isbn) {
-	this.isbn = isbn;
-}
+	public String getIsbn() {
+		return isbn;
+	}
 
-public boolean isPrice() {
-	return price;
-}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
 
-public void setPrice(boolean price) {
-	this.price = price;
-}
+	public double isPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [title=" + title + ", author=" + author + ", year=" + year + ", isbn=" + isbn + ", price=" + price
+				+ "]";
+	}
 
 }
